a603c4cbd87af39cfa2fef389121f7b5fc6d5683
mspokrovsk <enotestvaty@gmail.com>
2024-04-13 16:25:38 +0500

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Diplom_Pokrovskaya
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

1870697232815811f945d59085de8cdc860fb33d
mspokrovsk <enotestvaty@gmail.com>
2024-04-19 19:46:36 +0500

    Add new branch feature_api

---
+++ Tests/API_tests/GetTest.cs
@@ -0,0 +1,78 @@
+using Allure.NUnit.Attributes;
+using Allure.Net.Commons;
+using RestSharp;
+using System.Net;
+using NLog;
+using Allure.NUnit;
+using System.Text.Json.Serialization;
+
+namespace Diplom_Pokrovskaya.Tests.UI_tests
+{
+    [TestFixture]
+    [Parallelizable(scope: ParallelScope.All)]
+    [AllureNUnit]
+    [AllureEpic("API")]
+        
+    public class GetTest
+    {
+        protected readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private const string BaseRestUri = "https://mspokrovsk.testmo.net";
+
+        [Test(Description = "Проверка успешного ответа от сервера при запросе страницы с проектами")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureOwner("mspokrovsk")]
+        [AllureStory("API NFE")]
+
+        public void CheckSuccessfulResponse_WhenGetProjects()
+        {
+            const string endpoint = "/api/v1/projects?page=1";
+            const string token = "testmo_api_eyJpdiI6IkZGcDJ4M2JFTkFacCtBVG51dTZST2c9PSIsInZhbHVlIjoiQUZJbnlQVElTOVBockNDeVk5WVlqcHlPeTBpQis1bnpZb1hSbzUrVVR1Zz0iLCJtYWMiOiJiNzE5ZDEzZTc3OTgxYzliZmQzN2Q3OTFmNGY0ZGZkZGE1YTU4MzIyNWY0MDFhMDdkZjZlZjFlMzFiMzk3MzUxIiwidGFnIjoiIn0=";
+
+            // Setup Rest Client
+            var client = new RestClient(BaseRestUri);
+
+            // Setup Request
+            var request = new RestRequest(endpoint);
+
+            request.AddHeader("Authorization", $"Bearer {token}");
+
+            // Execute Request
+            var response = client.ExecuteGet(request);
+
+            Logger.Info(response.Content);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+        }
+
+        [Test(Description = "Проверка успешного ответа от сервера при запросе пользователя")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureOwner("mspokrovsk")]
+        [AllureStory("API NFE")]
+
+        public void CheckSuccessfulResponse_WhenGetUser()
+        {
+            const string endpoint = "/api/v1/users/{user_id}";
+            const string token = "testmo_api_eyJpdiI6IkZGcDJ4M2JFTkFacCtBVG51dTZST2c9PSIsInZhbHVlIjoiQUZJbnlQVElTOVBockNDeVk5WVlqcHlPeTBpQis1bnpZb1hSbzUrVVR1Zz0iLCJtYWMiOiJiNzE5ZDEzZTc3OTgxYzliZmQzN2Q3OTFmNGY0ZGZkZGE1YTU4MzIyNWY0MDFhMDdkZjZlZjFlMzFiMzk3MzUxIiwidGFnIjoiIn0=";
+
+            // Setup Rest Client
+            var client = new RestClient(BaseRestUri);
+
+            // Setup Request
+            var request = new RestRequest(endpoint)
+                .AddUrlSegment("user_id", 1); 
+
+            request.AddHeader("Authorization", $"Bearer {token}");
+
+            // Execute Request
+            var response = client.ExecuteGet(request);
+
+            Logger.Info(response.Content);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+        }
+
+    }
+}
+   
